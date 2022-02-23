32a439d0535cb3e239aebd693eebdb1b8acea565
brendan <lobrendan@gmail.com>
2022-02-22 16:36:10 -0800

    code ready for code review 1

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -10,9 +10,29 @@ describe("POST /", function () {
       productId: 1000,
       name: "Test Tester",
       addr: "100 Test St",
-      zip: "12345-6789",
+      zipcode: "12345-6789",
     });
 
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+
+  test("invalid inputs", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 900,
+      name: 123,
+      addr: 123.00,
+      zip: 12345
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error.message).toEqual(
+      [
+        "instance.productId must be greater than or equal to 1000",
+        "instance.name is not of a type(s) string",
+        "instance.addr is not of a type(s) string",
+        "instance is not allowed to have the additional property \"zip\"",
+        "instance requires property \"zipcode\""
+      ]
+    );
   });
 });

ba1215099409fb71f743616b225e817c85244397
Peter Loft <peterloft3@gmail.com>
2022-02-22 18:16:16 -0600

    completed installation of jsonschema for validation

--- routes/shipments.js
+++ routes/shipments.js
@@ -1,7 +1,12 @@
 "use strict";
 
+const jsonschema = require("jsonschema");
+const shipmentSchema = require("../schemas/shipmentSchema.json");
+
 const express = require("express");
 const router = new express.Router();
+
+const {BadRequestError} = require("../expressError");
 
 const { shipProduct } = require("../shipItApi");
 
@@ -14,8 +19,15 @@ const { shipProduct } = require("../shipItApi");
  */
 
 router.post("/", async function (req, res, next) {
-  const { productId, name, addr, zip } = req.body;
-  const shipId = await shipProduct({ productId, name, addr, zip });
+  const result = jsonschema.validate(req.body, shipmentSchema);
+
+  if (!result.valid) {
+    let errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const { productId, name, addr, zipcode } = req.body;
+  const shipId = await shipProduct({ productId, name, addr, zipcode });
   return res.json({ shipped: shipId });
 });
 
